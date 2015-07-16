79456926b40a7226a082bed0b1c9430bafab9f0e
Daniel Kendell <daniel@kendell.org>
2015-07-16 20:00:57 +0100

    Added InitResponseTranscoder Stage

---
+++ src/Http/Application/Stage/InitResponseTranscoder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mduk\Gowi\Http\Application\Stage;
+
+use Mduk\Gowi\Http\Application;
+use Mduk\Gowi\Http\Application\Stage;
+use Mduk\Gowi\Http\Request;
+use Mduk\Gowi\Http\Response;
+
+class InitResponseTranscoder implements Stage {
+
+  public function execute( Application $app, Request $req, Response $res ) {
+    $contentType = $app->getConfig( 'http.response.content_type', false );
+
+    if ( $contentType === false ) {
+      return;
+    }
+
+    $transcoders = $app->getConfig( 'http.response.transcoders' );
+    $transcoder = $app->getConfig( "transcoder.{$transcoders[ $contentType ]}" );
+    $app->setConfig( 'http.response.transcoder', $transcoder );
+  }
+
+}
