a727a65243d53c1765b436c15390e4e59a522777
Daniel Kendell <daniel@kendell.org>
2015-07-16 20:03:22 +0100

    Added DecodeRequestBody Stage

---
+++ src/Http/Application/Stage/DecodeRequestBody.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mduk\Gowi\Http\Application\Stage;
+
+use Mduk\Gowi\Http\Application;
+use Mduk\Gowi\Http\Application\Stage;
+use Mduk\Gowi\Http\Request;
+use Mduk\Gowi\Http\Response;
+
+class DecodeRequestBody implements Stage {
+
+  public function execute( Application $app, Request $req, Response $res ) {
+    $content = $req->getContent();
+    if ( $content ) {
+      $transcoder = $app->getConfig('http.request.transcoder');
+      $payload = $transcoder->decode( $content );
+      $app->setConfig( 'http.request.payload', $payload );
+    }
+  }
+
+}
