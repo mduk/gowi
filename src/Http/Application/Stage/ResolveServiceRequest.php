892d43baa7b099dfbb4c652e3dcf5aba68cbe66b
Daniel Kendell <daniel@kendell.org>
2015-07-16 20:17:15 +0100

    added ResolveServiceRequest Stage

---
+++ src/Http/Application/Stage/ResolveServiceRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mduk\Gowi\Http\Application\Stage;
+
+use Mduk\Gowi\Http\Application\Stage;
+use Mduk\Gowi\Http\Application;
+use Mduk\Gowi\Http\Request;
+use Mduk\Gowi\Http\Response;
+
+class ResolveServiceRequest implements Stage {
+  public function execute( Application $app, Request $req, Response $res ) {
+    $service = $app->getConfig( 'service.name' );
+    $call = $app->getConfig( 'service.call' );
+    $parameters = $app->getConfig( 'service.parameters', [] );
+
+    $serviceRequest = $app->getService( $service )
+      ->request( $call );
+
+    $requiredParameters = $serviceRequest->getRequiredParameters();
+    foreach ( $requiredParameters as $required ) {
+      if ( !isset( $parameters[ $required ] ) ) {
+        throw new \Exception( "SERVICE REQUEST: Parameter {$required} is required" );
+      }
+    }
+
+    foreach ( $parameters as $pk => $pv ) {
+      $serviceRequest->setParameter( $pk, $pv );
+    }
+
+    if ( $app->getConfig( 'http.request.payload', false ) ) {
+      $payload = $app->getConfig( 'http.request.payload' );
+      $serviceRequest->setPayload( $payload );
+    }
+
+    $app->setConfig( 'service.request', $serviceRequest );
+  }
+
+}
