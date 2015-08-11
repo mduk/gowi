50f4c14d2c4c141e287aee8c7b2d6fed0eec38f6
Daniel Kendell <daniel@kendell.org>
2015-08-11 23:06:33 +0100

    added MethodNotAllowed Respond Stage

---
+++ src/Http/Application/Stage/Respond/MethodNotAllowed.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mduk\Gowi\Http\Application\Stage\Respond;
+
+use Mduk\Gowi\Http\Application;
+use Mduk\Gowi\Http\Application\Stage;
+use Mduk\Gowi\Http\Request;
+use Mduk\Gowi\Http\Response;
+
+class MethodNotAllowed implements Stage {
+  public function execute( Application $app, Request $req, Response $res ) {
+    $res->setStatusCode( 405 );
+    $res->headers->set( 'Content-Type', 'text/plain' );
+    $res->setContent(
+      "405 Method Not Allowed\n" .
+      $req->getMethod() . ' is not allowed on ' . $req->getUri()
+    );
+    return $res;
+  }
+}
