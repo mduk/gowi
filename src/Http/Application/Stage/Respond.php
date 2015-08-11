5592f165408a3bc7eb8a88a48fe089cc563af9e3
Daniel Kendell <daniel@kendell.org>
2015-08-12 00:13:38 +0100

    i assume catching that exception made sense at one point. but somehow i doubt it.

--- src/Http/Application/Stage/Respond.php
+++ src/Http/Application/Stage/Respond.php
@@ -10,19 +10,8 @@ use Mduk\Gowi\Http\Response;
 class Respond implements Stage {
 
   public function execute( Application $app, Request $req, Response $res ) {
-    try {
-      $res->setContent( $app->getConfig( 'http.response.body' ) );
-      $res->headers->set( 'Content-Type', $app->getConfig( 'http.response.content_type' ) );
-    }
-    catch ( Application\Exception $e ) {
-      if ( $e->getCode() == Application\Exception::INVALID_CONFIG_KEY ) {
-        $res->setContent( '' );
-      }
-      else {
-        throw $e;
-      }
-    }
-
+    $res->setContent( $app->getConfig( 'http.response.body' ) );
+    $res->headers->set( 'Content-Type', $app->getConfig( 'http.response.content_type' ) );
     return $res;
   }
 
