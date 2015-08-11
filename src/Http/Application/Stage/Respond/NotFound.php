640a4b70e0bcd1d4d3e507b887c7278b8cd2799f
Daniel Kendell <daniel@kendell.org>
2015-08-11 23:22:13 +0100

    added the notfound respond stage. should have added that earlier

---
+++ src/Http/Application/Stage/Respond/NotFound.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mduk\Gowi\Http\Application\Stage\Respond;
+
+use Mduk\Gowi\Http\Application;
+use Mduk\Gowi\Http\Application\Stage;
+use Mduk\Gowi\Http\Request;
+use Mduk\Gowi\Http\Response;
+
+class NotFound implements Stage {
+  public function execute( Application $app, Request $req, Response $res ) {
+    $res->setStatusCode( 404 );
+    $res->headers->set( 'Content-Type', 'text/plain' );
+    $res->setContent(
+      "404 Not Found\n" .
+      $req->getUri()
+    );
+    return $res;
+  }
+}
