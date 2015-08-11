227aedc984a33296b509621d13c5a283a510cffd
Daniel Kendell <daniel@kendell.org>
2015-08-11 23:20:26 +0100

    added router application builder

---
+++ src/Http/Application/Builder/Router.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Mduk\Gowi\Http\Application\Builder;
+
+use Mduk\Application\Stage\InitRouter as InitRouterStage;
+use Mduk\Application\Stage\MatchRoute as MatchRouteStage;
+use Mduk\Application\Stage\SelectResponseType as SelectResponseTypeStage;
+use Mduk\Application\Stage\SelectRequestTranscoder as SelectRequestTranscoderStage;
+use Mduk\Application\Stage\InitResponseTranscoder as InitResponseTranscoderStage;
+use Mduk\Application\Stage\DecodeRequestBody as DecodeRequestBodyStage;
+
+use Mduk\Gowi\Http\Application\Builder as BuilderAbstract;
+
+use Mduk\Gowi\Http\Application;
+
+/**
+ * Build a Router Application.
+ *
+ * The Router Application simply uses a 'router' service
+ * to get information on what kind of Application should
+ * handle the routed Request.
+ */
+class Router extends BuilderAbstract {
+
+  protected $routes = [];
+
+  public function defineRoute( $type, $pathMethod, $config ) {
+    if ( is_array( $pathMethod ) && count( $pathMethod ) == 2 ) {
+      $method = $pathMethod[0];
+      $path = $pathMethod[1];
+    }
+    else if ( is_string( $pathMethod ) ) {
+      $method = 'GET';
+      $path = $pathMethod;
+    }
+    else {
+      throw new \InvalidArgumentException(
+        print_r( $pathMethod, true ) . ' is not acceptable as the $pathMethod argument'
+      );
+    }
+
+    if ( $this->getDebug() ) {
+      $this->getLogger()
+        ->debug( __CLASS__ . ": Defining route: {$type} {$method} {$path}" );
+    }
+
+    $this->routes[] = [
+      'type' => $type,
+      'pathmethod' => $pathMethod,
+      'config' => $config
+    ];
+  }
+
+  public function build( Application $app = null, array $config = [] ) {
+    $app = parent::build( $app, $config );
+
+    $app->addStage( new \Mduk\Gowi\Http\Application\Stage\InitRouter );
+    $app->addStage( new \Mduk\Gowi\Http\Application\Stage\MatchRoute );
+
+    $allRoutes = [];
+
+    if ( $this->getLogger() && $this->routes == [] ) {
+      $this->getLogger()->warning( __CLASS__ . ": No routes have been defined. This is going nowhere fast." );
+    }
+
+    foreach ( $this->routes as $routeSpec ) {
+      $builtRoutes = $this->getApplicationBuilderFactory()->get( $routeSpec['type'] )
+        ->buildRoutes( $routeSpec['pathmethod'], $routeSpec['config'] );
+      $allRoutes = array_replace_recursive( $allRoutes, $builtRoutes );
+    }
+
+    $app->setConfig( 'routes', $allRoutes );
+
+    return $app;
+  }
+
+}
---
+++ test/Http/Application/Builder/RouterTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Mduk\Gowi\Http\Application\Builder;
+
+use Mduk\Gowi\Http\Application;
+use Mduk\Gowi\Http\Application\Stage\Stub as StubStage;
+use Mduk\Gowi\Http\Application\Builder as ApplicationBuilder;
+use Mduk\Gowi\Http\Request;
+use Mduk\Gowi\Factory;
+
+class RouterTest extends \PHPUnit_Framework_TestCase {
+
+  public function testNotFound() {
+    $builder = new Router;
+    $app = $builder->build();
+    $response = $app->run( Request::create( '/foo' ) );
+
+    $this->assertEquals( 404, $response->getStatusCode() );
+  }
+
+  public function testMethodNotAllowed() {
+    $builderFactory = new Factory( [
+      'stub' => function() {
+        return new StubBuilder;
+      }
+    ] );
+
+    $builder = new Router;
+    $builder->setApplicationBuilderFactory( $builderFactory );
+
+    $builder->defineRoute( 'stub', '/foo', [] );
+    
+    $response = $builder->build()->run( Request::create( '/foo', 'POST' ) );
+
+    $this->assertEquals( 405, $response->getStatusCode() );
+  }
+
+  public function testOk() {
+    $builderFactory = new Factory( [
+      'stub' => function() {
+        return new StubBuilder;
+      }
+    ] );
+
+    $builder = new Router;
+    $builder->setApplicationBuilderFactory( $builderFactory );
+
+    $builder->defineRoute( 'stub', '/foo', [
+      'builder' => 'stub',
+      'config' => []
+    ] );
+    
+    $response = $builder->build()->run( Request::create( '/foo' ) );
+
+    $this->assertEquals( 200, $response->getStatusCode() );
+  }
+
+}
+
+class StubBuilder extends ApplicationBuilder {
+  public function buildRoutes( $path, $config ) {
+    return [
+      $path => [
+        'GET' => $config
+      ]
+    ];
+  }
+
+  public function build( Application $app = null, array $config = [] ) {
+    if ( !$app ) {
+      $app = parent::build( $app, $config );
+    }
+
+    $app->addStage( new StubStage( function( $app, $req, $res ) {
+      return $res->ok()->text( 'ok' );
+    }) );
+
+    return $app;
+  }
+}
