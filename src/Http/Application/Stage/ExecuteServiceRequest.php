df9c9a7cf63496659355f79fd1ab2a8059ec3323
Daniel Kendell <daniel_kendell@macdan.net>
2017-01-23 12:33:54 +0000

    Docs for Stage\ExecuteServiceRequest

--- src/Http/Application/Stage/ExecuteServiceRequest.php
+++ src/Http/Application/Stage/ExecuteServiceRequest.php
@@ -10,6 +10,33 @@ use Mduk\Gowi\Http\Application;
 use Mduk\Gowi\Http\Request;
 use Mduk\Gowi\Http\Response;
 
+/**
+ * Execute Service Request
+ *
+ * Execute the Prepared Service Request at [service.request] and assign the
+ * Service Response Result to [serivce.result], taking into account the
+ * [service.multiplicity] key.
+ *
+ * The [service.multiplicity] key defines our expectations for the Service Response
+ * Result and can have one of three values:
+ *
+ * None - We are not expecting a Service Response Result at all. An example of 
+ *        this might be a call that deletes an object. If a Multiplicity of None
+ *        is specified 
+ *
+ *  One - We are expecting one and only one result. Getting more than one result
+ *        back in the Service Response constitutes an error. An example of this
+ *        might be a 'getObjectById' query call, if there is more than one object
+ *        with that ID, then there has been an error somewhere.
+ *
+ * Many - No expectations are made on the length of the Service Response Result,
+ *        there could be one result, there could be twenty, there could be none.
+ *        No wrong answers.
+ *
+ * @getconfig service.request
+ * @getconfig service.multiplicity
+ * @setconfig service.result
+ */
 class ExecuteServiceRequest implements Stage {
   public function execute( Application $app, Request $req, Response $res ) {
     $multiplicity = $app->getConfig( 'service.multiplicity', 'many' );
