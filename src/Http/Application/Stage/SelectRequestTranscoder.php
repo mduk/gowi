9df7d82f402fc60bae28f2d33dcd1aec2611d62e
Daniel Kendell <daniel_kendell@macdan.net>
2017-01-23 16:44:07 +0000

    Docs for SelectResponseTranscoder

--- src/Http/Application/Stage/SelectRequestTranscoder.php
+++ src/Http/Application/Stage/SelectRequestTranscoder.php
@@ -7,6 +7,22 @@ use Mduk\Gowi\Http\Application\Stage;
 use Mduk\Gowi\Http\Request;
 use Mduk\Gowi\Http\Response;
 
+/**
+ * Select Request Transcoder
+ *
+ * If the Incoming HTTP Request contains no Request Body, then this stage has nothing to
+ * do and will simply return.
+ *
+ * For Incoming HTTP Requests that contain a Request Body, this stage will attempt to
+ * match the Incoming HTTP Request's Content-Type Header to a one of the available
+ * Request Transcoders as defined in [http.request.transcoders.*]. The selected
+ * Transcoder will be assigned to [http.request.transcoder].
+ *
+ * @getconfig http.request.transcoders.*
+ * @getconfig transcoders.*
+ * @setconfig http.request.content_type
+ * @setconfig http.request.transcoder
+ */
 class SelectRequestTranscoder implements Stage {
 
   public function execute( Application $app, Request $req, Response $res ) {
